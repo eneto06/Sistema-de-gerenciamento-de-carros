07a0e4b96a511f102e7cfab9352b29b507fb78e1
Eurico Neto <eurico.neto6@gmail.com>
2024-05-15 19:00:06 -0300

    Atualizando o arquivo forms.py e adicionando algumas validações para o model

--- cars/forms.py
+++ cars/forms.py
@@ -1,13 +1,23 @@
 from django import forms
-from cars.models import Brand
+from cars.models import Car
 
-class CarForm(forms.Form):
-    model = forms.CharField(max_length=200)
-    brand = forms.ModelChoiceField(Brand.objects.all())
-    factory_year = forms.IntegerField()
-    model_year = forms.IntegerField()
-    plate = forms.CharField(max_length=10)
-    value = forms.FloatField()
-    photo = forms.ImageField()
 
+class CarModelForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = '__all__'
+
+#Criando validações
+
+    def clean_value(self):
+        #Captura o valor 'value' do model
+        value = self.cleaned_data.get('value')
+        if value < 20000:
+            self.add_error('value', 'Valor mínimo do carro deve ser de R$20.000,00')
+        return value
     
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('value')
+        if factory_year < 1975:
+            self.add_error('factory_year', 'Não é possível cadastrar carros fabricados antes de 1975')
+        return factory_year
--- cars/views.py
+++ cars/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cars.models import Car
-from cars.forms import CarForm
+from cars.forms import CarModelForm
 
+# O usuário acessou o '/cars/' e ativou essa função 
 def cars_view(request):
 
-    cars = Car.objects.all()
+    # Essa variável percorre todo os objetos do model 'Car' (<Query set>)
+    cars = Car.objects.all().order_by('model')
+
+    # Variável que contém a busca feita pelo usuário 
     search = request.GET.get('search')
 
+    ''' Condição que verifica se a busca é nula,
+        se não for filter(model__contais = search)
+        busca a pesquisa feita pelo usuário
+    '''    
     if search:
         cars = Car.objects.filter(model__contains=search)
 
@@ -18,8 +26,17 @@ def cars_view(request):
 
 def new_car_view(request):
 
+    # Condição é atendida quando o usuário cadastra um carro novo
     if request.method == 'POST':
-        new_car_form = CarForm(request.POST, request.FILES)
+        # request.POST: pega as informações do POST que o usuário faz
+        # request.FILES: Pega os Arquivos que o usuário envia no metodo POST
+        new_car_form = CarModelForm(request.POST, request.FILES)
+        #Verifica se os dados do formulário estão válidos
+        if new_car_form.is_valid():
+            #Salva no banco de dados o nosso carro cadastrado
+            new_car_form.save()
+            #Redireciona o usuário para a lista de carros
+            return redirect('cars_list')
     else:
-        new_car_form = CarForm()
+        new_car_form = CarModelForm()
         return render(request, 'new_car.html', {'new_car_form': new_car_form})
