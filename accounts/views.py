aa58d573b8a8da03f7d451bb72c7d9921e0ba9f6
Eurico Neto <eurico.neto6@gmail.com>
2024-05-23 10:28:25 -0300

    Adicionando o class based views ao app cars

--- accounts/views.py
+++ accounts/views.py
@@ -4,26 +4,35 @@ from django.shortcuts import render, redirect
 
 
 def register_view(request):
-    if request.method == 'POST':    
+    #O if roda quando o usuário preenche os dados de login
+    if request.method == 'POST':
+        #Valida os dados do usuário    
         user_form = UserCreationForm(request.POST)
         if user_form.is_valid():
             user_form.save()
+            #Redireciona para a tela de login
             return redirect('login')
     else:
+        #Cria um formulário vazio para o nosso usuário
         user_form = UserCreationForm()
     return render(request, 'register.html', {'user_form': user_form})
 
 def login_view(request):
     if request.method == 'POST':
+        #Capturando duas variáveis
         username = request.POST["username"]
         password = request.POST["password"]
+        #Faz a autenticação do login do usuário
         user = authenticate(request, username = username, password = password)
+        #Se o usuário existe
         if user is not None:
+            #Faça login 
             login(request, user)
             return redirect('cars_list')
         else:
             login_form = AuthenticationForm()
     else:
+        #Renderiza o nosso form de login vazio
         login_form = AuthenticationForm()
     return render(request, 'login.html', {'login_form': login_form})
 
--- cars/views.py
+++ cars/views.py
@@ -1,42 +1,68 @@
+
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views import View
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator    
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
-# O usuário acessou o '/cars/' e ativou essa função 
-def cars_view(request):
+   
 
-    # Essa variável percorre todo os objetos do model 'Car' (<Query set>)
-    cars = Car.objects.all().order_by('model')
 
-    # Variável que contém a busca feita pelo usuário 
-    search = request.GET.get('search')
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
 
-    ''' Condição que verifica se a busca é nula,
-        se não for filter(model__contais = search)
-        busca a pesquisa feita pelo usuário
-    '''    
-    if search:
-        cars = Car.objects.filter(model__contains=search)
-
-    return render(
-        request, 
-        'cars.html', 
-        {'cars': cars} 
-        )
-
-def new_car_view(request):
-
-    # Condição é atendida quando o usuário cadastra um carro novo
-    if request.method == 'POST':
-        # request.POST: pega as informações do POST que o usuário faz
-        # request.FILES: Pega os Arquivos que o usuário envia no metodo POST
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        #Verifica se os dados do formulário estão válidos
-        if new_car_form.is_valid():
-            #Salva no banco de dados o carro cadastrado pelo usuário
-            new_car_form.save()
-            #Redireciona o usuário para a lista de carros
-            return redirect('cars_list')
-    else:
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
+        if search:
+            cars = cars.filter(model__icontains = search)
+        return cars
+    
+    
+class NewCarsView(View):
+    def get(self, request):
+        # Condição é atendida quando o usuário cadastra um carro novo
+        if request.method == 'POST':
+            # request.POST: pega as informações do POST que o usuário faz
+            # request.FILES: Pega os Arquivos que o usuário envia no metodo POST
+            new_car_form = CarModelForm(request.POST, request.FILES)
+            #Verifica se os dados do formulário estão válidos
+            if new_car_form.is_valid():
+                #Salva no banco de dados o carro cadastrado pelo usuário
+                new_car_form.save()
+                #Redireciona o usuário para a lista de carros
+                return redirect('cars_list')
+    def post(self, request):
         new_car_form = CarModelForm()
         return render(request, 'new_car.html', {'new_car_form': new_car_form})
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'
+    
+    def get_success_url(self):
+        return reverse_lazy('car_detail', kwargs = {'pk': self.object.pk})
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = 'car_delete.html'
+    success_url = '/cars/'
